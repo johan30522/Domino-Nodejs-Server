76bd7a465ceaf8888610fe3b148917fcab564d6c
Johan Arroyo <jjoharr@gmail.com>
2019-10-12 11:56:20 -0700

    segundo commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./config/middlewares');
 
 const PORT = process.env.PORT || 3000;
 
+//esto es una prueba
 
 app.listen(PORT, (error) => {
   if (error) console.error(`INTERNAL SERVER ERROR: ${error}`);
