65a25c3062b83b4be1217de82931c8934890bd2f
Johan Arroyo <jjoharr@gmail.com>
2019-11-03 09:58:11 -0800

    Se parametriza el url de frontend

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,18 +11,15 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const contactRoutes = require('../api/contact/routes');
 const cors = require('cors');
+//permite obtener la configuracion que utiliza el domino desde el config.json
+const config=require('./config');
 
-// environment variables
-process.env.NODE_ENV = 'development';
-
-app.use(cors({origin:'http://localhost:4200'}));//se agrega para que permita peticiones del servidor de desarrollo de angular en el 4200.
-
+app.use(cors({origin:global.gConfig.url_frontend}));//se agrega para que permita peticiones del servidor de angular.
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(logger('dev'));
 app.use('/api', contactRoutes);
-
 
 module.exports = app;
